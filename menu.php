e7a586c7b6b98a957654887819fee1957c5c41bf
Luferva <lfrv2005@gmail.com>
2022-10-17 15:56:48 -0300

    FOI ADICIONADO CSS

--- menu.php
+++ menu.php
@@ -1,29 +1,37 @@
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>OLX - Menu</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
-<?php
-require('config.php');
-require('verifica.php');
-?>
+    <?php
+    require('config.php');
+    require('verifica.php');
+    ?>
 
-<nav>
-    <a href="cadastro_anun.php">Cadastrar Anúncios</a>
-    <?php if ($_SESSION["UsuarioNivel"] == "ADM"){?><a href="cadastro_cat.php">Cadastrar categoria</a><?php }?>
-    <a href="cadastro_user.php">Cadastrar Usuário</a>
-    <a href="relatorio_anun.php">Relátorio de Anúncio</a>
-    <div >
-        <img src="uploads/<?php echo $_SESSION["avatar"];?>" width="100" >
-        <a href="logout.php"><img src="imagens/logout.png" alt="Logout" width="50"></a>
+     <h1>Bem Vindo ao OLX !!!</h1>   
+    <div id="profile">
+        <img src="uploads/<?php echo $_SESSION["avatar"]; ?>" width="140"><br>
         <strong><?php echo ($_SESSION["nome_user"]) ?></strong><br>
+        <a href="logout.php"><img src="imagens/logout.png" alt="Logout" width="50"></a><br>
+
     </div>
-    
-</nav>  
+    <br>
+    <nav>
+        <a href="cadastro_anun.php">Cadastrar Anúncios</a>
+        <?php if ($_SESSION["UsuarioNivel"] == "ADM") { ?><a href="cadastro_cat.php">Cadastrar categoria</a><?php } ?>
+        <a href="cadastro_user.php">Cadastrar Usuário</a>
+        <a href="relatorio_anun.php">Relátorio de Anúncio</a>
+
+
+
+    </nav>
 
 </body>
+
 </html>
